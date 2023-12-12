d9599b0dfbcbaaedf225b09dde48d78162e30f1a
antonovmike <antonovma@outlook.com>
2023-12-12 17:02:41 +0400

    Like-Unlike done

--- README.md
+++ README.md
@@ -32,8 +32,8 @@ Swap SECRET_KEY='dev' with some random bytes
 [Keep Developing!](https://flask.palletsprojects.com/en/3.0.x/tutorial/next/):
 
 - ✅ A detail view to show a single post. Click a post’s title to go to its page.
-- ➡️ Like / unlike a post.
-- Comments.
+- ✅ Like / unlike a post.
+- ➡️ Comments.
 - Tags. Clicking a tag shows all the posts with that tag.
 - A search box that filters the index page by name.
 - Paged display. Only show 5 posts per page.
--- flaskr/blog.py
+++ flaskr/blog.py
@@ -14,10 +14,14 @@ bp = Blueprint('blog', __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username, likes'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
+    # for post in posts:
+    #     post = dict(post)
+    #     post['likes'] = db.execute('SELECT likes FROM post WHERE id = ?', (post['id'],)).fetchone()['likes']
+    # return render_template('blog/index.html', posts=posts)
     return render_template('blog/index.html', posts=posts)
 
 
@@ -55,7 +59,7 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username'
+        'SELECT p.id, title, body, created, author_id, username, likes'
         ' FROM post p JOIN user u ON p.author_id = u.id'
         ' WHERE p.id = ?',
         (id,)
@@ -124,7 +128,8 @@ def like(id):
     db.commit()
     post = get_post(id)
     post = dict(post)
-    post['likes'] = db.execute('SELECT likes FROM post WHERE id = ?', (id,)).fetchone()['likes']
+    post['likes'] = db.execute('SELECT likes FROM post WHERE id = ?', (id,)).fetchone()[
+        'likes']
     return render_template('blog/post.html', post=post)
 
 

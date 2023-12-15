4b89e94a4f27e7adcc18f3befe1b5eadcc42447a
antonovmike <antonovma@outlook.com>
2023-12-15 11:49:26 +0400

    created class Post

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -11,22 +11,66 @@ from flaskr.db import get_db
 bp = Blueprint('blog', __name__)
 
 
+class Post:
+    def __init__(self, id, title, body, created, author_id, username, likes, comments):
+        self.id = id
+        self.title = title
+        self.body = body
+        self.created = created
+        self.author_id = author_id
+        self.username = username
+        self.likes = likes
+        self.comments = comments
+
+    @staticmethod
+    def get_posts():
+        db = get_db()
+        posts_data = db.execute(
+            'SELECT p.id, title, body, created, author_id, username, '
+            '(SELECT COUNT(*) FROM post_like WHERE post_id = p.id AND liked = TRUE) AS likes, '
+            '(SELECT COUNT(*) FROM comment WHERE post_id = p.id) AS comments '
+            'FROM post p JOIN user u ON p.author_id = u.id '
+            'ORDER BY created DESC'
+        ).fetchall()
+        return [Post(*post_data) for post_data in posts_data]
+
+    @staticmethod
+    def get_post(id, check_author=False):
+        post = get_db().execute(
+            'SELECT p.id, title, body, created, author_id, username, '
+            '(SELECT COUNT(*) FROM post_like WHERE post_id = p.id AND liked = TRUE) AS likes, '
+            '(SELECT COUNT(*) FROM comment WHERE post_id = p.id) AS comments '
+            'FROM post p JOIN user u ON p.author_id = u.id '
+            'WHERE p.id = ?',
+            (id, )
+        ).fetchone()
+
+        if post is None:
+            abort(404, f"Post id {id} doesn't exist.")
+
+        if check_author and post['author_id'] != g.user['id']:
+            abort(403)
+
+        comments = get_db().execute(
+            'SELECT c.id, body, created, author_id, username '
+            'FROM comment c JOIN user u ON c.author_id = u.id '
+            'WHERE post_id = ? '
+            'ORDER BY created DESC',
+            (id,)
+        ).fetchall()
+
+        return dict(post=post, comments=comments)
+
+
 @bp.route('/')
 def index():
-    db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, body, created, author_id, username, '
-        '(SELECT COUNT(*) FROM post_like WHERE post_id = p.id AND liked = TRUE) AS likes, '
-        '(SELECT COUNT(*) FROM comment WHERE post_id = p.id) AS comments '
-        'FROM post p JOIN user u ON p.author_id = u.id '
-        'ORDER BY created DESC'
-    ).fetchall()
+    posts = Post.get_posts()
     return render_template('blog/index.html', posts=posts)
 
 
 @bp.route('/<int:id>')
 def post(id):
-    post = get_post(id)
+    post = Post.get_post(id)
     return render_template('blog/post.html', post=post)
 
 
@@ -56,36 +100,10 @@ def create():
     return render_template('blog/create.html')
 
 
-def get_post(id, check_author=False):
-    post = get_db().execute(
-        'SELECT p.id, title, body, created, author_id, username, '
-        '(SELECT COUNT(*) FROM post_like WHERE post_id = p.id AND liked = TRUE) AS likes, '
-        '(SELECT COUNT(*) FROM comment WHERE post_id = p.id) AS comments '
-        'FROM post p JOIN user u ON p.author_id = u.id '
-        'WHERE p.id = ?',
-        (id, )
-    ).fetchone()
-
-    if post is None:
-        abort(404, f"Post id {id} doesn't exist.")
-
-    if check_author and post['author_id'] != g.user['id']:
-        abort(403)
-
-    comments = get_db().execute(
-        'SELECT c.id, body, created, author_id, username '
-        'FROM comment c JOIN user u ON c.author_id = u.id '
-        'WHERE post_id = ? '
-        'ORDER BY created DESC',
-        (id,)
-    ).fetchall()
-
-    return dict(post=post, comments=comments)
-
 @bp.route('/<int:id>/update', methods=('GET', 'POST'))
 @login_required
 def update(id):
-    post = get_post(id, check_author=True)
+    post = Post.get_post(id, check_author=True)
 
     if request.method == 'POST':
         title = request.form['title']
@@ -113,7 +131,7 @@ def update(id):
 @bp.route('/<int:id>/delete', methods=('GET', 'POST'))
 @login_required
 def delete(id):
-    get_post(id, check_author=True)
+    Post.get_post(id, check_author=True)
     db = get_db()
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
@@ -141,7 +159,7 @@ def like(id):
         )
 
     db.commit()
-    post = get_post(id)
+    post = Post.get_post(id)
     post = dict(post)
     post['likes'] = db.execute('SELECT COUNT(*) FROM post_like WHERE post_id = ? AND liked = TRUE', (id,)).fetchone()[0]
     return render_template('blog/post.html', post=post)

6ebf8a27289a3a270c4b681c9d84134162388d89
antonovmike <antonovma@outlook.com>
2023-12-28 21:45:50 +0400

    fixed large text, added an empty button to upload image

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -1,7 +1,6 @@
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
-from flask_sqlalchemy import pagination
 from werkzeug.exceptions import abort
 
 from datetime import datetime

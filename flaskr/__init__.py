6455664c52b85c0ef267c9004b70c12c97dddfff
antonovmike <antonovma@outlook.com>
2024-01-30 18:42:06 +0400

    added logger.info

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -27,7 +27,7 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/hello/')
     def hello():
-        return 'Hello, World!'
+        return 'Greetings on my demo blog!'
 
     with app.app_context():
         init_db()
